102f326b51a16d53aa7e67f7e81794a432031e73
jpitkanen18 <jpitkanen18@gmail.com>
2019-03-27 11:45:27 +0200

    Initial Commit

---
+++ iOSTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by Jese on 27/03/2019.
+//  Copyright © 2019 jpitkanen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
